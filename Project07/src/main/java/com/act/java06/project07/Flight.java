832298577bf709ebaf0aba2964af7c752aa40414
ct070227_huy <lehuy12052004@gmail.com>
2023-08-24 14:50:41 +0700

    lqh

---
+++ Project07/src/main/java/com/act/java06/project07/Flight.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.act.java06.project07;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Flight {
+    String flightCode;
+    String planeCode;
+    LocalDate 
+}
---
+++ Project07/src/main/java/com/act/java06/project07/IFlight.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.act.java06.project07;
+
+/**
+ *
+ * @author ADMIN
+ */
+public interface IFlight {
+    void add();
+    void remote();
+    void edit();
+    void selectAvailableFlight(); //dùng wildcard
+}
