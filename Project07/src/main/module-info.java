b80aba65d5f433cd5abf489b23622b306542abd3
hoang_htran <at190221@actvn.edu.vn>
2023-09-03 23:30:51 +0700

    delete

--- Project07/src/main/module-info.java
+++
@@ -1,9 +0,0 @@
-
-module com.act.java06.project07 {
-    requires com.google.gson;
-    requires com.fasterxml.uuid;
-    requires org.slf4j;
-    
-    exports com.act.java06.project07;
-    opens com.act.java06.project07 to com.google.gson;    
-}
