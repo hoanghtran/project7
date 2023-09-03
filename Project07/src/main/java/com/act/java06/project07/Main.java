91fbfe981e18e643ee6a3b158d685f078c91c01e
ct070227_huy <lehuy12052004@gmail.com>
2023-09-03 22:01:23 +0700

    lqh

--- Project07/src/main/java/com/act/java06/project07/Main.java
+++ Project07/src/main/java/com/act/java06/project07/Main.java
@@ -123,6 +123,26 @@ public class Main {
         }
     }
     
+    public static boolean isSubstring(String str1, String str2) {
+        int len1 = str1.length();
+        int len2 = str2.length();
+        
+        for (int i = 0; i <= len1 - len2; i++) {
+            int j;
+            for (j = 0; j < len2; j++) {
+                if (str1.charAt(i + j) != str2.charAt(j)) {
+                    break;
+                }
+            }
+            
+            if (j == len2) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
     static void generateTicketCode(String code, List<JSON.Airline> database) throws IOException{
         
         String rs = null;
