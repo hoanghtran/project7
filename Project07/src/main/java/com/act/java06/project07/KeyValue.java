710e65d72512efa19dafa87db6c24f7d29ed5990
DuyAT180613 <vuducduy17042003@gmail.com>
2023-09-03 21:47:29 +0700

    duy

---
+++ Project07/src/main/java/com/act/java06/project07/KeyValue.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.act.java06.project07;
+
+/**
+ *
+ * @author Admin
+ */
+public class KeyValue<K,V> {
+    private K key;
+    private V value;
+
+    public KeyValue(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+    
+}
