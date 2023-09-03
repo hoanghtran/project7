e7f20290113d488b7a978e8d7660819d2ac40253
hoang_htran <at190221@actvn.edu.vn>
2023-09-03 23:12:24 +0700

    hh

--- Project07/src/main/java/com/act/java06/project07/Main.java
+++ Project07/src/main/java/com/act/java06/project07/Main.java
@@ -124,7 +124,6 @@ public class Main {
         }
     }
 
-<<<<<<< HEAD
     public static boolean isSubstring(String str1, String str2) {
         int len1 = str1.length();
         int len2 = str2.length();
@@ -143,85 +142,69 @@ public class Main {
         }
 
         return false;
-=======
+    }
+
     static void getEmptySeats(ArrayList<KeyValue<String, Integer>> list, String code) throws IOException {
         List<JSON.Airline> database = new ArrayList<>();
         database = read_json_file(Json_file_path);
         for (int i = 1; i <= list.size(); i++) {
-            int x=i+1;
-            String str=null;
-            if(i<=25){
-                if(i<10){
-                    str = "A"+"0"+i;
+            int x = i + 1;
+            String str = null;
+            if (i <= 25) {
+                if (i < 10) {
+                    str = "A" + "0" + i;
+                } else {
+                    str = "A" + i;
                 }
-                else{
-                    str = "A"+i;
+            } else if (i <= 50) {
+                if (i % 25 < 10) {
+                    str = "B" + "0" + i % 25;
+                } else {
+                    str = "B" + i % 25;
                 }
-            }
-            else if(i<=50){
-                if(i%25<10){
-                    str = "B"+"0"+i%25;
+            } else if (i <= 75) {
+                if (i % 25 < 10) {
+                    str = "C" + "0" + i % 25;
+                } else {
+                    str = "C" + i % 25;
                 }
-                else{
-                    str = "B"+i%25;
+            } else if (i <= 100) {
+                if (i % 25 < 10) {
+                    str = "D" + "0" + i % 25;
+                } else {
+                    str = "D" + i % 25;
                 }
-            }
-            else if(i<=75){
-                if(i%25<10){
-                    str = "C"+"0"+i%25;
+            } else if (i <= 125) {
+                if (i % 25 < 10) {
+                    str = "E" + "0" + i % 25;
+                } else {
+                    str = "E" + i % 25;
                 }
-                else{
-                    str = "C"+i%25;
+            } else if (i <= 150) {
+                if (i % 25 < 10) {
+                    str = "F" + "0" + i % 25;
+                } else {
+                    str = "F" + i % 25;
                 }
-            }
-            else if(i<=100){
-                if(i%25<10){
-                    str = "D"+"0"+i%25;
+            } else if (i <= 175) {
+                if (i % 25 < 10) {
+                    str = "G" + "0" + i % 25;
+                } else {
+                    str = "G" + i % 25;
                 }
-                else{
-                    str = "D"+i%25;
-                }
-            }
-            else if(i<=125){
-                if(i%25<10){
-                    str = "E"+"0"+i%25;
-                }
-                else{
-                    str = "E"+i%25;
-                }
-            }
-            else if(i<=150){
-                if(i%25<10){
-                    str = "F"+"0"+i%25;
-                }
-                else{
-                    str = "F"+i%25;
-                }
-            }
-            else if(i<=175){
-                if(i%25<10){
-                    str = "G"+"0"+i%25;
-                }
-                else{
-                    str = "G"+i%25;
-                }
-            }
-            else if(i<=200){
-                if(i%25<10){
-                    str = "H"+"0"+i%25;
-                }
-                else{
-                    str = "H"+i%25;
+            } else if (i <= 200) {
+                if (i % 25 < 10) {
+                    str = "H" + "0" + i % 25;
+                } else {
+                    str = "H" + i % 25;
                 }
             }
             list.get(i).setValue(0);
             list.get(i).setKey(str);
         }
 
-        
     }
 
-    
     static int getIndexOfAirline(String code) throws IOException {// error
         List<JSON.Airline> database = new ArrayList<>();
         database = read_json_file(Json_file_path);
@@ -232,7 +215,6 @@ public class Main {
 
         }
         return -1;
->>>>>>> 4915d615c4c6355b5b2b6162bc7419b9dd398f54
     }
 
     static void generateTicketCode(String code, List<JSON.Airline> database) throws IOException {
@@ -246,7 +228,6 @@ public class Main {
             }
         }
         // duyet cac so 
-        for
     }
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
@@ -302,7 +283,7 @@ public class Main {
                                     plane.setNumOfSeats(num);
                                 }
 
-                                database.add(new JSON.Airline(brandname, code, numOfPlanes, listOfPlanes, numOfPlanes, flights));
+                                database.add(new JSON.Airline(brandname, code, numOfPlanes, listOfPlanes, numOfPlanes));
                                 write_airlines_file(Json_file_path, database);
                                 break;
                             case 2: // xóa một hãng hàng không 
