ae8698701b2cf842c4b1585537bb6c2a9c90607c
DuyAT180613 <vuducduy17042003@gmail.com>
2023-09-05 18:09:08 +0700

    duy

---
+++ Project07/src/main/java/com/act/java06/project07/swing/main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.act.java06.project07.swing;
+
+/**
+ *
+ * @author Admin
+ */
+public class main {
+    public static void main(String[] args) {
+        luaChonDoiTuong luaChon = new luaChonDoiTuong();
+        luaChon.setVisible(true);
+        luaChon.setLocationRelativeTo(null);
+    }
+}
